22440fcf9d115edc14e1c7abf6def3d79f5e0480
GoodNight0721 <2908322103@qq.com>
2026-08-21 15:54:04 +0800

    update a simpler version of 1193_template

--- pandas/financial/time_series data pandas/1193_template.py
+++ pandas/financial/time_series data pandas/1193_template.py
@@ -1,29 +1,20 @@
 import pandas as pd
 
 def monthly_transactions(transactions: pd.DataFrame) -> pd.DataFrame:
-    transactions['trans_date'] = transactions['trans_date'].dt.to_period('M').astype(str)
+    transactions['month'] = transactions['trans_date'].dt.to_period('M').astype(str)
     transactions['approved'] = (transactions['state'] == 'approved').astype(int)
     transactions['approved_amount'] = transactions['amount'] * transactions['approved']
     
     transactions = (
         transactions
-        .groupby(['country', 'trans_date'], dropna=False)
-        .agg({
-            'state': 'count',
-            'approved': 'sum',
-            'amount': 'sum',
-            'approved_amount': 'sum'
-        })
-        .reset_index()
-        .rename(
-            columns={
-                'state': 'trans_count',
-                'approved': 'approved_count',
-                'amount': 'trans_total_amount',
-                'approved_amount': 'approved_total_amount',
-                'trans_date': 'month'
-            }
+        .groupby(['country', 'month'], dropna=False)
+        .agg(
+            trans_count = ('id', 'count'),
+            approved_count = ('approved', 'sum'),
+            trans_total_amount = ('amount', 'sum'),
+            approved_total_amount = ('approved_amount', 'sum')
         )
+        .reset_index()
     )
     
     return transactions
