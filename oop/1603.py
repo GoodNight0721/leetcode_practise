35135685ff1277096a46518b71aa461091ee6a09
GoodNight0721 <2908322103@qq.com>
2026-08-24 15:19:45 +0800

    1603.py

---
+++ oop/1603.py
@@ -0,0 +1,24 @@
+class ParkingSystem:
+
+    def __init__(self, big: int, medium: int, small: int):
+        self.big = big
+        self.medium = medium
+        self.small = small
+        
+    def addCar(self, carType: int) -> bool:
+        if self.big > 0 and carType == 1:
+            self.big -= 1
+            return True
+        if self.medium > 0 and carType == 2:
+            self.medium -= 1
+            return True
+        if self.small > 0 and carType == 3:
+            self.small -= 1
+            return True
+        return False
+            
+
+
+# Your ParkingSystem object will be instantiated and called as such:
+# obj = ParkingSystem(big, medium, small)
+# param_1 = obj.addCar(carType)
