be0ac7719b1f9dbe8cae8bc8672727e234ed3d5b
GoodNight0721 <2908322103@qq.com>
2026-09-08 18:42:38 +0800

    122

---
+++ 6_动态规划/122.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        cash = 0
+        hold = -prices[0]
+
+        for price in prices[1:]:
+            new_cash = max(cash, hold + price)
+            new_hold = max(hold, cash - price)
+            cash = new_cash
+            hold = new_hold
+        
+        return cash
