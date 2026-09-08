c627386b2ac077e9f5bda7066a02fa33f299625d
GoodNight0721 <2908322103@qq.com>
2026-09-08 11:10:42 +0800

    45_template

---
+++ 4_贪心/45_template.py
@@ -0,0 +1,13 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        farthest = 0
+        end = 0
+        cnt = 0
+        for i in range(len(nums)-1):
+            farthest = max(farthest, i+nums[i])
+
+            if i == end:
+                cnt += 1
+                end = farthest
+        
+        return cnt
